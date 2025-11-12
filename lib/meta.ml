035c88f3b77ebdd107cb2aac28104dc034c8c4f2
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-12 15:51:51 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/meta.ml
+++ lib/meta.ml
@@ -1,12 +1,11 @@
 let app = "oca"
 let version = "0.0.1"
-let title = "OCaml Compiler Suite"
+let title = "OCaml Compiler Architecture"
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
 let license = "MIT"
 let github = "git@gitflic.ru:dponyatov/oca.git"
 
-let readme = 
+(* let readme =
   let f = open_out "README.md" in
-  Printf.fprintf f "# `${app}"
-  close_out f
+  Printf.fprintf f "# `${app}" close_out f *)
