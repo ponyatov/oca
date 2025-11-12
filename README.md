7ea4f57d9c91bfc52b860a0a227ca98fb4367cc5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-12 10:38:11 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- README.md
+++
@@ -1,6 +0,0 @@
-# `oca` 0.0.1
-## OCaml Compiler Suite
-
-(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2025 MIT
-
-gitflic: git@gitflic.ru:dponyatov/oca.git
--- lib/oca.ml
+++ lib/oca.ml
@@ -3,4 +3,10 @@ let version = "0.0.1"
 let title = "OCaml Compiler Suite"
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
+let license = "MIT"
+let github = "git@gitflic.ru:dponyatov/oca.git"
 
+let readme = 
+  let f = open_out "README.md" in
+  Printf.fprintf f "# `${app}"
+  close_out f
