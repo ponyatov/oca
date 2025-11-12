700d3d1bcbebcbeb60fd503ec37ad4000d16a561
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-12 15:57:55 +0400

    .

---
+++ README.md
@@ -0,0 +1,6 @@
+# ![](doc/logo.png) `oca` 0.0.1
+## OCaml Compiler Architecture
+
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2025 MIT
+
+github: https://github.com/ponyatov/oca
--- lib/meta.ml
+++ lib/meta.ml
@@ -1,11 +1,22 @@
+let logo = "doc/logo.png"
 let app = "oca"
 let version = "0.0.1"
 let title = "OCaml Compiler Architecture"
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
+let year = 2025
 let license = "MIT"
-let github = "git@gitflic.ru:dponyatov/oca.git"
+let gitflic = "git@gitflic.ru:dponyatov/oca.git"
+let github = "https://github.com/ponyatov/oca"
 
-(* let readme =
+let readme () =
   let f = open_out "README.md" in
-  Printf.fprintf f "# `${app}" close_out f *)
+  Printf.fprintf f "# ![](%s) `%s` %s
+## %s
+
+(c) %s <<%s>> %i %s
+
+github: %s
+" logo app version title author email year license github;
+  close_out f
+(* readme () *)
