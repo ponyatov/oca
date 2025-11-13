0a5fcdf071c02b398a0150f5cace80d43ca3b718
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-13 16:32:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lib/formout.ml
@@ -0,0 +1,9 @@
+
+open Format
+
+open_box 0;
+print_string "x =";
+print_space ();
+print_int 1;
+close_box ();
+print_newline ();
--- mk/ocaml.mk
+++ mk/ocaml.mk
@@ -1,24 +1,29 @@
-ocaml: $(UTOP) $(DUNE) $(OFMT) $(OLSP) .ocamlformat
-
-$(UTOP): $(OPAM) $(OCAMLC)
-	opam install -y utop && touch $@
-$(DUNE): $(OPAM) $(OCAMLC)
-	opam install -y dune && touch $@
-$(OFMT): $(OPAM) $(OCAMLC)
-	opam install -y ocamlformat && touch $@
-$(OLSP): $(OPAM) $(OCAMLC)
-	opam install -y ocaml-lsp-server && touch $@
-$(CAMLP5): $(OPAM) $(OCAMLC)
-	opam install -y camlp5 && touch $@
-
-$(OCAMLC): $(OPAM)
-# 	opam switch list-available ; opam switch list
-	opam switch create cs3110 ocaml-base-compiler.$(OCAML_VER) && touch $@
-	opam switch set    cs3110 ; eval $(opam env)
+ocaml: $(DUNE) $(UTOP) $(OFMT) $(OLSP)
 
 $(OPAM):
 	bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
 	opam init -a
 
+$(OCAMLC): $(OPAM)
+	opam switch set default ; eval $(opam env --switch=default)
+# 	opam switch create $(OCAML_VER) ocaml-base-compiler.$(OCAML_VER)
+# 	opam switch set $(OCAML_VER) ; eval $(opam env --switch=$(OCAML_VER))
+# 	opam switch create 5.3.0 ocaml-base-compiler.5.3.0
+# 	opam switch set 5.3.0 ; eval $(opam env --switch=5.3.0)
+# 	opam switch list-available ; opam switch list
+# 	opam switch create cs3110 ocaml-base-compiler.$(OCAML_VER) && touch $@
+# 	opam switch set    cs3110 ; eval $(opam env)
+
+$(DUNE) $(UTOP) $(OFMT) $(OLSP): $(OCAMLC)
+	opam install -y dune utop ocamlformat ocaml-lsp-server
+	touch $(DUNE) $(UTOP) $(OFMT) $(OLSP)
+	$(MAKE) .ocamlformat .ocamlinit
+
+$(CAMLP5): $(OCAMLC)
+	opam install -y camlp5 && touch $@
+
 .ocamlformat:
 	echo "version = `ocamlformat --version`" > $@
+
+.ocamlinit:
+	echo "#use "topfind";;" > $@
